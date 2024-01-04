2e232ad9654963a3ae277f159435802ef76f1d01
rayan_aouf <rayanaouf1512@gmail.com>
2024-01-04 19:09:19 +0100

    daracademyV5.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,6 +111,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage")
+    implementation("com.google.firebase:firebase-analytics")
 
 
 
